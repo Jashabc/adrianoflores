0feeee66a70ad8a428465dc9d72b11f0611d0214
Jashabc <joseaneirah007@gmail.com>
2024-09-22 00:05:01 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+  // Eliminar la clase "container" del body
+  document.body.classList.remove("container");
+  
+  // Mostrar un mensaje
+  alert("¡Te quiero Angela <3.!");
+};
